21d516ce37b3a2351de9dc49563b3990a990cb0a
faizal.a <faizal.a@inapp.com>
2017-06-28 19:12:39 +0530

    @HostListener + @HostBinding Fn

--- src/app/app.directive.ts
+++ src/app/app.directive.ts
@@ -1,26 +1,50 @@
 import {Directive, ElementRef, HostListener, HostBinding, Input} from '@angular/core';
 
 @Directive({
-  selector: '[appDirective]'
+  selector: '[appDirective]',
+  host: {
+    '(click)': 'onDirectiveClickRef($event)'
+  }
 })
 
 export class AppDirective {
   constructor(public elemRef: ElementRef) {
   }
 
+  /******************************************************
+   *Passing Component function declaration into Directive
+   *****************************************************/
   @Input('appDirective') appDirectiveRef;
   @Input('secondProperty') secondPropertyRef;
 
-  /* @HostBinding [Default Attribute Setup - cust_bool_prop] => Console */
-  @HostBinding("attr.cust_bool_prop") cust_bool_prop=true;
+  /********************************************************************************
+   *@HostListener [Passing Component function declaration into Attribute Directive]
+   ********************************************************************************/
+  @HostListener("click") onAppDirectiveOnClick() {
+   if (this.secondPropertyRef) {
+   //Appling Styles - ElementRef
+   this.elemRef.nativeElement.style.color = this.secondPropertyRef.color;
+   console.log("Binding Second Property >> ", this.secondPropertyRef);
+   }
+   //Component Function - Reference
+   this.appDirectiveRef();
 
-  @HostListener("click")onAppDirectiveOnClick() {
-    console.log(this, "\n Directive Fn Init. >> Calling the bind function associated with 'appDirective' attribute");
-    //Component Function - Reference
-    this.appDirectiveRef();
-    //Appling Styles - ElementRef
-    this.elemRef.nativeElement.style.color = this.secondPropertyRef ? this.secondPropertyRef.color:'orange';
-    this.cust_bool_prop =!this.cust_bool_prop;
+   this.cust_bool_prop = !this.cust_bool_prop;
+   }
+
+  /***********************************************************************
+   *@HostBinding [Default Attribute Setup - 'cust_bool_prop' (true/false)]
+   * Default - true
+   ***********************************************************************/
+  @HostBinding("attr.cust_bool_prop") cust_bool_prop = true;
+
+  /*******************************************************************************************************
+   *Directive Host Events [Check Directive Decorator Metadata]
+   *Note : Comment out >> @HostListener [Passing Component function declaration into Attribute Directive]
+   *After done checking revert back (Uncomment @HostListener)
+   *******************************************************************************************************/
+  onDirectiveClickRef(elemRef) {
+    console.log("Directive Click Handler Init. >> host:{(click):Fn()}")
   }
 
 }
